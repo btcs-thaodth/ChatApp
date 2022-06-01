9b18c8a2fcc8aa7f794421e70d3a64b604431bd7
Thảo Đỗ Hồng <thao.dh@neo-lab.vn>
2022-06-01 10:55:35 +0700

    message disconnection

--- web/src/component/Header/index.tsx
+++ web/src/component/Header/index.tsx
@@ -1,11 +1,22 @@
+import { handleLoginLogout } from '../../server/chatapp';
+import { v4 as uuid } from 'uuid';
+import { useRecoilState } from 'recoil';
+import { currentUserStore } from '../../store/CurrentUser';
+import { useNavigate } from 'react-router-dom';
 interface Props{
     title: string
 }
 const Header = ({title}: Props) => {
+    const navigate = useNavigate();
+    const [currentUser] = useRecoilState(currentUserStore)
+    const handleDisconnect = () => {
+        handleLoginLogout('disconnectedToServer',uuid(), currentUser);
+        navigate('/');
+    }
     return(
         <div className="flex justify-between flex-shrink-0 px-8 py-4 border-b border-gray-300">
 				<h1 className="text-xl font-semibold">{title}</h1>
-				{/* <button className="flex items-center h-8 px-2 text-sm bg-gray-300 rounded-sm hover:bg-gray-400">New post</button> */}
+				<button className="flex items-center h-8 px-2 text-sm bg-gray-300 rounded-sm hover:bg-gray-400" onClick={handleDisconnect}>Disconnection</button>
 			</div>
     )
 }
